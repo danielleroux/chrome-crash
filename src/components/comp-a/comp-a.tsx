2320e71d82eed1f115b4ce99a84d83b98e15415e
Daniel Leroux <daniel.leroux@siemens.com>
2024-07-05 14:09:27 +0200

    fix: comp

--- src/components/comp-a/comp-a.tsx
+++ src/components/comp-a/comp-a.tsx
@@ -10,15 +10,17 @@ export class CompA {
 
   @State() isResized = false;
 
+  /** has nothing to do with the issue */
   @Listen('resize', { target: 'window' })
   onResize() {
     this.isResized = true;
   }
+  /** has nothing to do with the issue */
 
   render() {
     return (
       <Host>
-        <p>Make sure you have dev tools open before enter the side OR resize it ones after open the dev tools</p>
+        <p>Make sure you have dev tools open before enter the localhost:3333 OR resize it ones after open the dev tools</p>
         <button onClick={() => (this.doTheCrash = !this.doTheCrash)}>{!this.isResized ? 'Resize window via dev Console' : 'Crash the chrome now!'}</button>
         {this.doTheCrash ? (
           <div>
